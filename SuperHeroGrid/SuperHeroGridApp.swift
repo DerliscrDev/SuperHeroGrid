978a09d590559a2ca29ca4c938a699686e2b3e4a
Derlis Cañete <derliscrdev@gmail.com>
2023-02-02 19:37:54 -0300

    initial commit

---
+++ SuperHeroGrid/SuperHeroGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroGridApp.swift
+//  SuperHeroGrid
+//
+//  Created by Derlis Ramón Cañete Ríos on 2023-02-02.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
